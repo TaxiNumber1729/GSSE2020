cdaf0a79d1925b8be07713dc14ef448b163212e5
Duy Tran <35908342+TaxiNumber1729@users.noreply.github.com>
2020-07-01 09:49:23 -0500

    Add files via upload

---
+++ t.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm 
+
+window = tk.Tk()
+
+label = tk.Label(text='Distribution of Traffic Deaths in Memphis', width = 40, height = 40)
+label.pack()
+
+
+def graph():
+    deaths= np.random.normal(35000,100,10000)
+    plt.hist(deaths, 50)
+    plt.xlabel('Number of Deaths')
+    plt.ylabel('Percentage of Deaths Caused by Traffic Accidents')
+    plt.show()
+
+button = tk.Button(text='Simulate', command=graph)
+button.pack()
+
+
+window.mainloop()
